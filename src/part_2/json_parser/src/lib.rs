7dbdf68bcb1b895ea93c3d30280ac1a3f5178b01
Connor Haskins <ConnorAHaskins@gmail.com>
2023-07-10 18:37:46 -0700

    parse json as one collection

--- src/part_2/json_parser/src/lib.rs
+++ src/part_2/json_parser/src/lib.rs
@@ -3,15 +3,18 @@ use std::io::{Result, Error, ErrorKind::InvalidData};
 
 #[derive(PartialEq, Debug)]
 pub enum Value<'a> {
-    ObjectOpen,
-    ObjectClose,
-    ArrayOpen,
-    ArrayClose,
+    Collection{ values: JsonCollection<'a> },
     Key(&'a str),
     String(&'a str),
     Number(f32),
     Boolean(bool),
     Null,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct JsonCollection<'a> {
+    elements: Vec<Value<'a>>,
+    key_val_pairs: bool,
 }
 
 // TODO: Can't return a reference to json string that will go out of scope
@@ -20,55 +23,66 @@ pub enum Value<'a> {
 //     parse_json_str(&json)
 // }
 
-#[allow(dead_code)] // TODO: Remove this
-fn parse_json_str<'a>(json_str: &'a str) -> Result<Vec<Value>> {
-    let json_chars = json_str.as_bytes();
-    let mut values: Vec<Value> = Vec::new();
-    let mut i = 0;
-
-    let mut inside_object_stack: Vec<bool> = Vec::new();
-
-    while i < json_chars.len() {
-        match json_chars[i] {
+fn parse_json_collection<'a>(json_str: &'a str, json_bytes: &'a [u8], index: usize, key_val_pairs: bool) -> Result<(Value<'a>, usize)> {
+    let mut collection = JsonCollection{ elements: Vec::new(), key_val_pairs: key_val_pairs };
+    
+    let mut i = index;
+    while i < json_bytes.len() {
+        match json_bytes[i] {
             b'{' => {
-                values.push(Value::ObjectOpen);
-                inside_object_stack.push(true);
                 i += 1;
+                match parse_json_collection(json_str, json_bytes, i, true) {
+                    Ok((value, new_i)) => {
+                        collection.elements.push(value);
+                        i = new_i;
+                    },
+                    Err(e) => { return Err(e); }
+                }
             },
             b'}' => {
-                values.push(Value::ObjectClose);
-                assert!(inside_object_stack.pop() == Some(true));
                 i += 1;
+                let is_object = key_val_pairs;
+                assert!(is_object);
+                let value = Value::Collection{ values: collection };
+                return Ok((value, i));
             },
             b'[' => {
-                values.push(Value::ArrayOpen);
-                inside_object_stack.push(false);
                 i += 1;
+                match parse_json_collection(json_str, json_bytes, i, false) {
+                    Ok((value, new_i)) => {
+                        collection.elements.push(value);
+                        i = new_i;
+                    },
+                    Err(e) => { return Err(e); }
+                }
             },
             b']' => {
-                values.push(Value::ArrayClose);
-                assert!(inside_object_stack.pop() == Some(false));
                 i += 1;
+                let is_array = !key_val_pairs;
+                assert!(is_array);
+                let value = Value::Collection{ values: collection };
+                return Ok((value, i));
             },
             b'"' => {
-                 let is_key = match values.last() {
-                    Some(Value::Key(_)) => false,
-                    _ => { 
-                        inside_object_stack.last() == Some(&true)
+                let is_key = if key_val_pairs { 
+                    match collection.elements.last() {
+                        Some(Value::Key(_)) => false,
+                        _ => true
                     }
-                };
+                } else { false };
+                
                 let mut escaped = false;
                 let open_quote_index = i;
                 i += 1;
-                while i < json_chars.len() {
-                    match json_chars[i] {
+                while i < json_bytes.len() {
+                    match json_bytes[i] {
                         b'\\' => { escaped = true; }
                         b'"' => {
                             if !escaped {
                                 if is_key { 
-                                    values.push(Value::Key(&json_str[open_quote_index+1..i])); 
+                                    collection.elements.push(Value::Key(&json_str[open_quote_index+1..i])); 
                                 } else { 
-                                    values.push(Value::String(&json_str[open_quote_index+1..i])); 
+                                    collection.elements.push(Value::String(&json_str[open_quote_index+1..i])); 
                                 }
                                 i += 1;
                                 break;
@@ -80,10 +94,10 @@ fn parse_json_str<'a>(json_str: &'a str) -> Result<Vec<Value>> {
                 }
             },
             b't' => {
-                if let Some(b'r') = json_chars.get(i+1) {
-                    if let Some(b'u') = json_chars.get(i+2) {
-                        if let Some(b'e') = json_chars.get(i+3) {
-                            values.push(Value::Boolean(true));
+                if let Some(b'r') = json_bytes.get(i+1) {
+                    if let Some(b'u') = json_bytes.get(i+2) {
+                        if let Some(b'e') = json_bytes.get(i+3) {
+                            collection.elements.push(Value::Boolean(true));
                             i += 3;
                             continue;
                         }
@@ -92,11 +106,11 @@ fn parse_json_str<'a>(json_str: &'a str) -> Result<Vec<Value>> {
                 return Err(Error::new(InvalidData, "ERROR: Parsing invalid token 't'"));
             },
             b'f' => {
-                if let Some(b'a') = json_chars.get(i+1) {
-                    if let Some(b'l') = json_chars.get(i+2) {
-                        if let Some(b's') = json_chars.get(i+3) {
-                            if let Some(b'e') = json_chars.get(i+4) {
-                                values.push(Value::Boolean(false));
+                if let Some(b'a') = json_bytes.get(i+1) {
+                    if let Some(b'l') = json_bytes.get(i+2) {
+                        if let Some(b's') = json_bytes.get(i+3) {
+                            if let Some(b'e') = json_bytes.get(i+4) {
+                                collection.elements.push(Value::Boolean(false));
                                 i += 4;
                                 continue;
                             }
@@ -106,10 +120,10 @@ fn parse_json_str<'a>(json_str: &'a str) -> Result<Vec<Value>> {
                 return Err(Error::new(InvalidData, "ERROR: Parsing invalid token 'f'"));
             },
             b'n' => {
-                if let Some(b'u') = json_chars.get(i+1) {
-                    if let Some(b'l') = json_chars.get(i+2) {
-                        if let Some(b'l') = json_chars.get(i+3) {
-                            values.push(Value::Null);
+                if let Some(b'u') = json_bytes.get(i+1) {
+                    if let Some(b'l') = json_bytes.get(i+2) {
+                        if let Some(b'l') = json_bytes.get(i+3) {
+                            collection.elements.push(Value::Null);
                             i += 3;
                             continue;
                         }
@@ -120,11 +134,11 @@ fn parse_json_str<'a>(json_str: &'a str) -> Result<Vec<Value>> {
             b'0'..=b'9'|b'-'|b'+' => { // TODO: Numbers will need more validation. This is just grabbing characters in the value slot where a number should be.
                 let first_number_index = i;
                 i += 1;
-                while i < json_chars.len() {
-                    match json_chars[i] {
+                while i < json_bytes.len() {
+                    match json_bytes[i] {
                         b'0'..=b'9'|b'.'|b'+'|b'-'|b'e'|b'E' => {},
                         _ => {
-                            values.push(Value::Number(json_str[first_number_index..i].parse::<f32>().unwrap()));
+                            collection.elements.push(Value::Number(json_str[first_number_index..i].parse::<f32>().unwrap()));
                             break;
                         }
                     }
@@ -138,9 +152,45 @@ fn parse_json_str<'a>(json_str: &'a str) -> Result<Vec<Value>> {
         }
     }
 
-    assert!(inside_object_stack.len() == 0);
+    return Err(Error::new(InvalidData, "ERROR: End of object or array not found."));
+}
 
-    Ok(values)
+#[allow(dead_code)] // TODO: Remove this
+fn parse_json_str<'a>(json_str: &'a str) -> Result<Value> {
+    let json_bytes = json_str.as_bytes();
+    
+    let mut root: Value = Value::Null;
+    let mut i = 0;    
+    while i < json_bytes.len() {
+        match json_bytes[i] {
+            b'{' => {
+                i += 1;
+                match parse_json_collection(json_str, &json_bytes, i, true) {
+                    Ok((value, new_i)) => {
+                        root = value;
+                        i = new_i;
+                    },
+                    Err(e) => { return Err(e); }
+                }
+            }
+            b'[' => {
+                i += 1;
+                match parse_json_collection(json_str, &json_bytes, i, false) {
+                    Ok((value, new_i)) => {
+                        root = value;
+                        i = new_i;
+                    },
+                    Err(e) => { return Err(e); }
+                }
+            }
+            b' ' | b'\t' | b'\n' | b'\r' => { i += 1; }, // ignore whitespace, commas, colons
+            _ => { 
+                return Err(Error::new(InvalidData, "ERROR: Json must have an object or array as the root. Nothing may exist but whitespace after the root.")); 
+            }
+        }
+    }
+
+    Ok(root)
 }
 
 #[cfg(test)]
@@ -151,95 +201,126 @@ mod tests {
     #[test]
     fn parse_nothing() {
         let json_str: &'static str = "";
-        let result = parse_json_str(json_str);
-        match result {
-            Ok(empty_vec) => {
-                assert_eq!(empty_vec.len(), 0);
-            },
-            Err(e) => { panic!("ERROR: {}", e); }
+        match parse_json_str(json_str) {
+            Ok(Value::Null) => {},
+            _ => { panic!("ERROR: Expected null value"); }
         }
     }
 
     #[test]
     fn parse_empty_object() {
         let json_str: &'static str = "{}";
-        let result = parse_json_str(json_str);
-        match result {
-            Ok(empty_object) => {
-                assert_eq!(empty_object.len(), 2);
-                assert_eq!(empty_object[0], Value::ObjectOpen);
-                assert_eq!(empty_object[1], Value::ObjectClose);
+        match parse_json_str(json_str) {
+            Ok(Value::Collection{ values: vals }) => {
+                assert_eq!(vals.elements.len(), 0);
+                assert_eq!(vals.key_val_pairs, true);
             },
             Err(e) => { panic!("ERROR: {}", e); }
+            _ => { panic!("ERROR: Expected empty object."); }
         }
     }
 
     #[test]
     fn parse_empty_array() {
         let json_str: &'static str = "[]";
-        let result = parse_json_str(json_str);
-        match result {
-            Ok(empty_array) => {
-                assert_eq!(empty_array.len(), 2);
-                assert_eq!(empty_array[0], Value::ArrayOpen);
-                assert_eq!(empty_array[1], Value::ArrayClose);
+        match parse_json_str(json_str) {
+            Ok(Value::Collection{ values: vals }) => {
+                assert_eq!(vals.elements.len(), 0);
+                assert_eq!(vals.key_val_pairs, false);
             },
-            Err(e) => { panic!("ERROR: {}", e); }
+            _ => { panic!("ERROR: Expected empty array."); }
         }
     }
 
     #[test]
     fn parse_object_w_empty_array() {
         let json_str: &'static str = "{\"empty_array\":[]}";
-        let result = parse_json_str(json_str);
-        match result {
-            Ok(object_w_empty_array) => {
-                assert_eq!(object_w_empty_array.len(), 5);
-                assert_eq!(object_w_empty_array[0], Value::ObjectOpen);
-                assert_eq!(object_w_empty_array[1], Value::Key("empty_array"));
-                assert_eq!(object_w_empty_array[2], Value::ArrayOpen);
-                assert_eq!(object_w_empty_array[3], Value::ArrayClose);
-                assert_eq!(object_w_empty_array[4], Value::ObjectClose);
+        match parse_json_str(json_str) {
+            Ok(Value::Collection{ values: vals }) => {
+                // assert root is an object that contains two elements (a empty array and its key)
+                assert_eq!(vals.elements.len(), 2);
+                assert_eq!(vals.key_val_pairs, true);
+                assert_eq!(vals.elements[0], Value::Key("empty_array"));
+                match &vals.elements[1] {
+                    Value::Collection{ values: vals } => {
+                        assert_eq!(vals.elements.len(), 0);
+                        assert_eq!(vals.key_val_pairs, false);
+                    },
+                    _ => { panic!("ERROR: Expected empty array."); }
+                }
             },
-            Err(e) => { panic!("ERROR: {}", e); }
+            _ => { panic!("ERROR: Expected empty object."); }
         }
     }
 
     #[test]
     fn parse_pairs_test() {
-        let json_str: &'static str = 
-            "{\"pairs\":[{\"x0\":12.5,\"y0\":24.5,\"x1\":50.5,\"y1\":37.5},{\"x0\":12.25,\"y0\":22.25,\"x1\":-17.25,\"y1\":3.525e-9}]}";
-        let result = parse_json_str(json_str);
-        match result {
-            Ok(object_with_pairs) => {
-                assert_eq!(object_with_pairs.len(), 25);
-                assert_eq!(object_with_pairs[0], Value::ObjectOpen);
-                assert_eq!(object_with_pairs[1], Value::Key("pairs"));
-                assert_eq!(object_with_pairs[2], Value::ArrayOpen);
-                assert_eq!(object_with_pairs[3], Value::ObjectOpen);
-                assert_eq!(object_with_pairs[4], Value::Key("x0"));
-                assert_eq!(object_with_pairs[5], Value::Number(12.5));
-                assert_eq!(object_with_pairs[6], Value::Key("y0"));
-                assert_eq!(object_with_pairs[7], Value::Number(24.5));
-                assert_eq!(object_with_pairs[8], Value::Key("x1"));
-                assert_eq!(object_with_pairs[9], Value::Number(50.5));
-                assert_eq!(object_with_pairs[10], Value::Key("y1"));
-                assert_eq!(object_with_pairs[11], Value::Number(37.5));
-                assert_eq!(object_with_pairs[12], Value::ObjectClose);
-                assert_eq!(object_with_pairs[13], Value::ObjectOpen);
-                assert_eq!(object_with_pairs[14], Value::Key("x0"));
-                assert_eq!(object_with_pairs[15], Value::Number(12.25));
-                assert_eq!(object_with_pairs[16], Value::Key("y0"));
-                assert_eq!(object_with_pairs[17], Value::Number(22.25));
-                assert_eq!(object_with_pairs[18], Value::Key("x1"));
-                assert_eq!(object_with_pairs[19], Value::Number(-17.25));
-                assert_eq!(object_with_pairs[20], Value::Key("y1"));
-                assert_eq!(object_with_pairs[21], Value::Number(3.525e-9));
-                assert_eq!(object_with_pairs[22], Value::ObjectClose);
-                assert_eq!(object_with_pairs[23], Value::ArrayClose);
-                assert_eq!(object_with_pairs[24], Value::ObjectClose);
+        let json_str: &'static str = "\
+            {\t\
+                \"pairs\" : [\
+                    {\
+                        \"x0\":12.5,\
+                        \"y0\":24.5,\
+                        \"x1\":50.5,\
+                        \"y1\":37.5\
+                    },\
+                    {\
+                        \"x0\":12.25,\
+                        \"y0\":22.25,\
+                        \"x1\":-17.25,\
+                        \"y1\":3.525e-9\
+                    }\r\
+                ]\n\
+            }";
+        
+        let root = parse_json_str(json_str);
+        match root {
+            Ok(Value::Collection{ values: vals }) => {
+                assert_eq!(vals.key_val_pairs, true); // collection is an object
+                assert_eq!(vals.elements.len(), 2); // collection contains array and its key
+                assert_eq!(vals.elements[0], Value::Key("pairs"));
+                let pairs_array = &vals.elements[1];
+                match pairs_array {
+                    Value::Collection{ values: vals } => {
+                        assert_eq!(vals.key_val_pairs, false); // collection is array
+                        assert_eq!(vals.elements.len(), 2); // collection has two pairs
+                        let pair_1 = &vals.elements[0];
+                        match pair_1 {
+                            Value::Collection{ values: vals } => {
+                                assert_eq!(vals.key_val_pairs, true); // collection is object
+                                assert_eq!(vals.elements.len(), 8); // collection has pairs of coordinates and keys
+                                assert_eq!(vals.elements[0], Value::Key("x0"));
+                                assert_eq!(vals.elements[1], Value::Number(12.5));
+                                assert_eq!(vals.elements[2], Value::Key("y0"));
+                                assert_eq!(vals.elements[3], Value::Number(24.5));
+                                assert_eq!(vals.elements[4], Value::Key("x1"));
+                                assert_eq!(vals.elements[5], Value::Number(50.5));
+                                assert_eq!(vals.elements[6], Value::Key("y1"));
+                                assert_eq!(vals.elements[7], Value::Number(37.5));
+                            },
+                            _ => { panic!("ERROR: Expected coordinate pair."); }
+                        }
+                        let pair_2 = &vals.elements[1];
+                        match pair_2 {
+                            Value::Collection{ values: vals } => {
+                                assert_eq!(vals.key_val_pairs, true); // collection is object
+                                assert_eq!(vals.elements.len(), 8); // collection has pairs of coordinates and keys
+                                assert_eq!(vals.elements[0], Value::Key("x0"));
+                                assert_eq!(vals.elements[1], Value::Number(12.25));
+                                assert_eq!(vals.elements[2], Value::Key("y0"));
+                                assert_eq!(vals.elements[3], Value::Number(22.25));
+                                assert_eq!(vals.elements[4], Value::Key("x1"));
+                                assert_eq!(vals.elements[5], Value::Number(-17.25));
+                                assert_eq!(vals.elements[6], Value::Key("y1"));
+                                assert_eq!(vals.elements[7], Value::Number(3.525e-9));
+                            },
+                            _ => { panic!("ERROR: Expected coordinate pair."); }
+                        }
+                    },
+                    _ => { panic!("ERROR: Expected array of pairs"); }
+                }
             },
-            Err(e) => { panic!("ERROR: {}", e); }
+            _ => { panic!("ERROR: Expected object with pairs array."); }
         }
     }
 }
