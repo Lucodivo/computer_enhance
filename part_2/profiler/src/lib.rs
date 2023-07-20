850943748b9a2b5d5ad7254c40483b7f6d75fc9e
Connor Haskins <ConnorAHaskins@gmail.com>
2023-07-20 14:52:28 -0700

    time_function/time_block macros + defer test

--- part_2/profiler/src/lib.rs
+++ part_2/profiler/src/lib.rs
@@ -1,46 +1,99 @@
-use clocks;
+use clocks::{measure_cpu_freq, read_cpu_timer, clocks_to_secs};
+pub use utils::Defer;
 
-struct Defer<F> 
-    where F: Fn() -> () 
-{ 
-    op: F 
+enum ProfilerNode { 
+    Open { stamp: u64, name: &'static str }, 
+    Close { stamp: u64, name: &'static str }
+}
+pub struct Profiler { creation_stamp: u64, cpu_freq: u64, nodes: Vec<ProfilerNode> }
+impl Profiler {
+    fn init(&mut self) {
+        self.cpu_freq = measure_cpu_freq(100);
+        self.creation_stamp = read_cpu_timer();
+    }
+    fn deinit(&self) {
+        let now = read_cpu_timer();
+        let total_clocks = now - self.creation_stamp;
+        let total_secs = clocks_to_secs(total_clocks, self.cpu_freq);        
+
+        let mut open_nodes: Vec<&ProfilerNode> = Vec::new();
+        for node in &self.nodes {
+            match node {
+                ProfilerNode::Open{ .. } => {
+                    open_nodes.push(&node);
+                },
+                ProfilerNode::Close{ stamp: close_stamp, name: close_name } => {
+                    if let ProfilerNode::Open{ stamp: open_stamp, name: open_name } = open_nodes.pop().expect("A profiler was unregistered without being registered.") {
+                        assert!(open_name == close_name, "Unmatched names for register and unregister found.");
+                        let delta_clocks = close_stamp - open_stamp;
+                        let percent = delta_clocks as f64 / total_clocks as f64 * 100.0;
+                        println!("{}: {} cycles ({:.2}%)", open_name, delta_clocks, percent);
+                    } else { panic!("A profiler was unregistered without being registered."); } 
+                }
+            }
+        }
+        assert!(open_nodes.len() == 0, "A profiler was registered without being unregistered.");
+
+        println!("Total: {} cycles ({} seconds)", total_clocks, total_secs);
+    }
+    pub fn register(&mut self, name: &'static str) {
+        self.nodes.push(ProfilerNode::Open{ stamp: read_cpu_timer(), name: name });
+    }
+    pub fn unregister(&mut self, name: &'static str) {
+        self.nodes.push(ProfilerNode::Close{ stamp: read_cpu_timer(), name: name });
+    }
 }
 
-impl<F> Drop for Defer<F> 
-    where F: Fn() -> () 
-{
-    fn drop(&mut self) { (self.op)(); }
-}
+pub static mut PROFILER: Profiler = Profiler{ creation_stamp: 0, cpu_freq: 0, nodes: Vec::<ProfilerNode>::new() };
 
 /*
 Calling this macro twice in the same function will *NOT* compile.
 */
-macro_rules! defer_print_func_name {
+#[macro_export]
+macro_rules! time_function {
     // `()` indicates that the macro takes no argument.
     () => {
         fn f() {}
-        fn type_name_of<T>(_: T) -> &'static str {
-            std::any::type_name::<T>()
-        }
+        fn type_name_of<T>(_: T) -> &'static str { std::any::type_name::<T>() }
         let name = type_name_of(f);
         let func_name_start = &name[..name.len() - 3].rfind(':').expect("Failed to find function name.") + 1;
         let func_name_end = name.len() - 3; // remove the trailing "::f"
-        let _defer_print = Defer { op: || { println!("{} has exited scope.", &name[func_name_start..func_name_end]); } }; 
+        let func_name = &name[func_name_start..func_name_end];
+
+        unsafe { PROFILER.register(func_name); }
+
+        let _defer_unregister = Defer::new(|| {
+            unsafe { PROFILER.unregister(func_name); }
+        });
     }
 }
 
-fn profiler_setup() {
-    defer_print_func_name!();
+#[macro_export]
+macro_rules! time_block {
+    // `()` indicates that the macro takes no argument.
+    ( $msg:expr ) => {
+        unsafe { PROFILER.register($msg); }
+
+        let _defer_unregister = Defer::new(|| {
+            unsafe { PROFILER.unregister($msg); }
+        });
+    }
 }
 
-fn profiler_teardown() {
-    defer_print_func_name!();
+pub fn profiler_setup() {
+    unsafe { PROFILER.init(); }
+    time_function!();
+}
+
+pub fn profiler_teardown() {
+    // NOTE: profiling teardown with PROFILER does not really work because the profiler will be closed on exit.
+    unsafe { PROFILER.deinit(); }
 }
 
 pub fn test() {
     profiler_setup();
-    defer_print_func_name!();
     {
+        time_block!("print hello block");
         println!("Hello, from non-macro println!");
     }
     profiler_teardown();
