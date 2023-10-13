64a238a6602949d49c1f5cc4db75e6f7569d5dbd
Connor Haskins <ConnorAHaskins@gmail.com>
2023-10-13 16:12:03 -0700

    repetition test repeats indefinitely

--- part_3/repetition_testing/src/main.rs
+++ part_3/repetition_testing/src/main.rs
@@ -1,4 +1,4 @@
-static TEST_FILE : &str = "../data/seed_123123/data_1000000_flex.json";
+static TEST_FILE : &str = "../../part_2/data/seed_123123/data_1000000_flex.json";
 
 struct RepetitionTestData {
     iterations: u64,
@@ -98,19 +98,19 @@ impl MeasureableAction for FSReadAsStringRepetition {
     fn tag(&self) -> &'static str{ "fs::read_as_string" }
 }
 
-struct FSReadBufferNoAllocRepetition {
+struct FSFileNoAllocationRepetition {
     file: Option<std::fs::File>,
     file_buffer: Vec<u8>,
 }
 
-impl FSReadBufferNoAllocRepetition {
+impl FSFileNoAllocationRepetition {
     fn new() -> Self {
         let file_metadata = std::fs::metadata(TEST_FILE).expect("Couldn't get fs::metadata for file");
         Self { file: None, file_buffer: Vec::with_capacity(file_metadata.len() as usize), }
     }
 }
 
-impl MeasureableAction for FSReadBufferNoAllocRepetition {
+impl MeasureableAction for FSFileNoAllocationRepetition {
     fn action(&mut self) -> MeasureableActionResults {
         use std::io::Read;
         self.file = Some(std::fs::File::open(TEST_FILE).expect("Couldn't fs::read file"));
@@ -132,68 +132,76 @@ impl MeasureableAction for FSReadBufferNoAllocRepetition {
 fn main() {
     let cpu_freq = clocks::measure_cpu_freq(1000);
 
-    let max_iterations: u64 = 10;
-    let max_secs: u64 = 2;
+    let max_iterations: u64 = 100;
+    let max_secs: u64 = 5;
     let max_clocks: u64 = max_secs * cpu_freq;
 
     let mut measureable_actions: Vec<Box<dyn MeasureableAction>> = Vec::new();
     measureable_actions.push(Box::new(FSReadRepetition::new()));
     measureable_actions.push(Box::new(FSFileRepetition::new()));
     measureable_actions.push(Box::new(FSReadAsStringRepetition::new()));
-    measureable_actions.push(Box::new(FSReadBufferNoAllocRepetition::new()));
+    measureable_actions.push(Box::new(FSFileNoAllocationRepetition::new()));
+    let mut repetition_test_data = Vec::<RepetitionTestData>::new();
+    repetition_test_data.resize_with(measureable_actions.len(), || RepetitionTestData {
+        iterations: 0,
+        total_clocks: 0,
+        min_clocks: u64::MAX,
+        max_clocks: 0
+    });
         
-    for measureable_action in &mut measureable_actions {
-        let mut rtd = RepetitionTestData {
-            iterations: 0,
-            total_clocks: 0,
-            min_clocks: u64::MAX,
-            max_clocks: 0
-        };
-        let mut running_iterations: u64 = 0;
-        let mut running_clocks: u64 = 0;
-        let mut byte_count;
+    loop { // loop until user force quits
+        let mut repetitions = measureable_actions.iter_mut().zip(repetition_test_data.iter_mut());
+        for (action, rtd) in &mut repetitions {
+            println!("--- {} ---", (*action).tag());
 
-        loop {
-            let starting_stamp = clocks::read_cpu_timer();
-            let results = (*measureable_action).action();
-            let ending_stamp = clocks::read_cpu_timer();
-            let diff_stamp = ending_stamp - starting_stamp;
+            let mut running_iterations: u64 = 0;
+            let mut running_clocks: u64 = 0;
+            let mut byte_count;
 
-            byte_count = results.byte_count;
+            loop {
+                let starting_stamp = clocks::read_cpu_timer();
+                let results = (*action).action();
+                let ending_stamp = clocks::read_cpu_timer();
+                let diff_stamp = ending_stamp - starting_stamp;
 
-            rtd.iterations += 1;
-            rtd.total_clocks += diff_stamp;
+                byte_count = results.byte_count;
 
-            running_iterations += 1;
-            running_clocks += diff_stamp;
+                rtd.iterations += 1;
+                rtd.total_clocks += diff_stamp;
 
-            (*measureable_action).reset();
+                running_iterations += 1;
+                running_clocks += diff_stamp;
 
-            if diff_stamp < rtd.min_clocks  {
-                rtd.min_clocks = diff_stamp;
-                running_clocks = 0;
-                running_iterations = 0;
-            } else if diff_stamp > rtd.max_clocks  {
-                rtd.max_clocks = diff_stamp;
+                (*action).reset();
+
+                if diff_stamp < rtd.min_clocks  {
+                    rtd.min_clocks = diff_stamp;
+                    running_clocks = 0;
+                    running_iterations = 0;
+                } else if diff_stamp > rtd.max_clocks  {
+                    rtd.max_clocks = diff_stamp;
+                }
+
+                let gigabyte_count = byte_count as f64 / 1024.0 / 1024.0 / 1024.0;
+                let min_time = rtd.min_clocks as f64 / cpu_freq as f64;
+                let min_bandwidth = gigabyte_count / min_time;
+                print!("\rMin: {} clocks ({:.6}ms) {:.6}gb/s", rtd.min_clocks, min_time, min_bandwidth);
+
+                if running_iterations >= max_iterations && running_clocks >= max_clocks {
+                    println!();
+                    break;
+                }
             }
 
-            if running_iterations >= max_iterations && running_clocks >= max_clocks {
-                break;
-            }
+            let gigabyte_count = byte_count as f64 / 1024.0 / 1024.0 / 1024.0;
+            let max_time = rtd.max_clocks as f64 / cpu_freq as f64;
+            let max_bandwidth = gigabyte_count / max_time;
+            let avg_clocks = rtd.total_clocks as f64 / rtd.iterations as f64;
+            let avg_time = avg_clocks as f64 / cpu_freq as f64;
+            let avg_bandwidth = gigabyte_count / avg_time;
+
+            println!("Max: {} clocks ({:.6}ms) {:.6}gb/s", rtd.max_clocks, max_time, max_bandwidth);
+            println!("Avg: {} clocks ({:.6}ms) {:.6}gb/s", avg_clocks, avg_time, avg_bandwidth);
         }
-
-        let gigabyte_count = byte_count as f64 / 1024.0 / 1024.0 / 1024.0;
-        let min_time = rtd.min_clocks as f64 / cpu_freq as f64;
-        let min_bandwidth = gigabyte_count / min_time;
-        let max_time = rtd.max_clocks as f64 / cpu_freq as f64;
-        let max_bandwidth = gigabyte_count / max_time;
-        let avg_clocks = rtd.total_clocks as f64 / rtd.iterations as f64;
-        let avg_time = avg_clocks as f64 / cpu_freq as f64;
-        let avg_bandwidth = gigabyte_count / avg_time;
-
-        println!("--- {} ---", (*measureable_action).tag());
-        println!("Min: {} ({:.6}) {:.6}gb/s", rtd.min_clocks, min_time, min_bandwidth);
-        println!("Max: {} ({:.6}) {:.6}gb/s", rtd.max_clocks, max_time, max_bandwidth);
-        println!("Avg: {} ({:.6}) {:.6}gb/s", avg_clocks, avg_time, avg_bandwidth);
     }
 }
